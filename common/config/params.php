7965f9a559a4c5d4f8765f8c1270200a09261ebf
Andrii Vasyliev <sol@hiqdev.com>
2015-08-18 17:07:02 +0000

    + orgName and orgUrl params

--- common/config/params.php
+++ common/config/params.php
@@ -10,6 +10,8 @@
  */
 
 return [
+    'orgName'      => 'AHnames',
+    'orgUrl'       => 'https://ahnames.com/',
     'supportEmail' => 'support@ahnames.com',
     'adminEmail'   => 'support@ahnames.com',
 ];
