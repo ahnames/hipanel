1e16a50a2dc2f975d4be96876913502770d3dd78
Andrii Vasyliev <sol@hiqdev.com>
2015-08-22 19:44:03 +0000

    + ProjectAsset

---
+++ frontend/assets/ProjectAsset.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace project\frontend\assets;
+
+use yii\web\AssetBundle;
+
+class ProjectAsset extends AssetBundle
+{
+    public $sourcePath = '@project/frontend/assets/ProjectAssetFiles';
+
+    public $css = [
+        'css/project.css',
+    ];
+}
