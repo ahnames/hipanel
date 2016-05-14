a270af83c56fd6137067c7b7f8bb474a16f97994
Andrii Vasyliev <sol@hiqdev.com>
2016-05-14 05:10:31 +0000

    inited tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+Yii::setAlias('@ahnames/hipanel', dirname(__DIR__) . '/src');
