2255aaceb47152d23e126c7ac94aa84984242d33
Andrii Vasyliev <sol@hiqdev.com>
2017-02-07 13:45:11 +0000

    emptied hisite config: removed use of assets and translations

--- src/config/hisite.php
+++ src/config/hisite.php
@@ -8,22 +8,4 @@
  * @copyright Copyright (c) 2015-2017, AHnames (https://ahnames.com/)
  */
 
-
-return [
-    'components' => [
-        'themeManager' => [
-            'assets' => [
-                \ahnames\hipanel\assets\ProjectAsset::class,
-                \ahnames\assets\ahnames\AhnamesAsset::class,
-            ],
-        ],
-        'i18n' => [
-            'translations' => [
-                'ahnames:hipanel' => [
-                    'class' => \yii\i18n\PhpMessageSource::class,
-                    'basePath' => '@ahnames/hipanel/messages',
-                ],
-            ],
-        ],
-    ],
-];
+return [];
