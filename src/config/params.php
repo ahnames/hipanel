8ef1834f21df735aaf63d74e04992d59c1edbd48
Andrii Vasyliev <sol@hiqdev.com>
2017-10-04 11:05:02 +0200

    added `ticket.canSetSpent` param

---
+++ src/config/params.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * AHnames implementation of HiPanel
+ *
+ * @link      https://ahnames.com/
+ * @package   hipanel
+ * @license   proprietary
+ * @copyright Copyright (c) 2015-2017, AHnames (https://ahnames.com/)
+ */
+
+return [
+    'ticket.canSetSpent' => null,
+];
